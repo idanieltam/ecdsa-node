47139c08677a4e85ebe54359d400fe28da3d4525
Daniel Tam <50918767+idanieltam@users.noreply.github.com>
2023-03-02 18:19:15 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -42,3 +42,20 @@ function setInitialBalance(address) {
     balances[address] = 0;
   }
 }
+
+/* credentials
+
+➜  scripts git:(main) ✗ node generate.js
+privateKey: fccf15448daa48e412c0d72551b8a006aa07140f36fb516ee0a62890d5ad05b3
+publicKey: 04fc51638d3647b00f100a41e906915da218880869f27c7420a53e1368aed7b2aa3cb102c14819f703bee6df5bd5cfd4d9408ff8c5e46ce11e1a8edfb57e3e6acf
+Address: 0xd3a86612e77295539656
+➜  scripts git:(main) ✗ node generate.js
+privateKey: aefef66dbdc4ab1345d845981b57b9bf231ec7eea0ae71748d5fe61f13cda51d
+publicKey: 043ac7b1147796a3cea0c59232de7e56e2b25d7f75da2fe72406599edf71e41ca1ee9285e9052be2a1f4c9a8874de391da36f3d40459f0b509451e2286e98522cc
+Address: 0x9d9e6f1312b5d7e321c7
+➜  scripts git:(main) ✗ node generate.js
+privateKey: 807d577872f9256a2c5265fc82b0afb37a65a7bbfa3d6604bd4ef66076ab78f7
+publicKey: 04d1199e942b5ef0076c27668a6032e67cdc92b703f41948733880dd0e03acf589e20a59b9ec5673c72b939d73ab728772257ae3ed373acc3d8b86a443bee656c8
+Address: 0x4f3e7f750a10daff15c6
+
+*/
